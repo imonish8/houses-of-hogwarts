226ed871f2ed71844e445c9b08201cd6adaa66b1
Monish Nule <imonishnule8@gmail.com>
2024-09-12 08:46:59 +0530

    Sept 2024

---
+++ houses/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="#" target="_blank">
+      <img src="/pngegg-2.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="#" target="_blank">
+      <img src="/pngegg-3.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="#" target="_blank">
+      <img src="/pngegg.svg" class="logo" alt="Vite logo" />
+    </a>
+    
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Which House you Truely Belong to" />
+  <audio
+        controls 
+        src="/Still.mp3">
+            
+            <code>audio</code> element.
+    </audio>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ houses/src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+   <form action="input">
+    <input type="text" class="user-month"> 
+    <button>
+      Submit You Birth Month
+    </button>
+   </form>
+   
+</template>
+<script>
+const userMonth = document.getElementsByClassName('user-month')
+ 
+if(userMonth == "January"){
+  alert("Belongs to Ravenclaw")
+ }
+else if(userMonth == "Feburary")
+   alert("Belongs to Ravenclaw")
+
+else if(userMonth == "March")
+   alert("Belongs to Ravenclaw")
+
+else if(userMonth == "April")
+   alert("Belongs to Ravenclaw")
+
+else if(userMonth == "May")
+   alert("Belongs to Ravenclaw")
+
+else if(userMonth == "June")
+   alert("Belongs to Ravenclaw")
+
+else if(userMonth == "July")
+   alert("Belongs to Ravenclaw")
+
+else if(userMonth == "August")
+   alert("Belongs to Ravenclaw")
+   
+else if(userMonth == "September")
+   alert("Belongs to Ravenclaw")
+
+
+</script>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
